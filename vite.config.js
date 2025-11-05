9e0cd5220267e46aa05f65a5d57af4b12818902f
MaThanhNhi <mathanhnhi2004@gmail.com>
2025-11-05 18:53:18 +0700

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './WAD-IA03-Sum-Calculator-App-Using-React/',
+  base: '/WAD-IA03-Sum-Calculator-App-Using-React/',
 })
